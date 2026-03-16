d11817524c00dc6e9bfc74cd61f88e989f158a7f
Ahn Yeonjeong <149382038+annmmww@users.noreply.github.com>
2026-03-16 23:42:24 +0900

    [level 3] Title: 베스트앨범, Time: 0.35 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -0,0 +1,31 @@
+function solution(genres, plays) {
+    const genreTotal = new Map();
+    const songsByGenre = new Map();
+    
+    for (let i = 0; i < genres.length; i++) {
+        const genre = genres[i];
+        const play = plays[i];
+        
+        genreTotal.set(genre, (genreTotal.get(genre) || 0) + play);
+        
+        if (!songsByGenre.has(genre)) {
+            songsByGenre.set(genre, []);
+        }
+        
+        songsByGenre.get(genre).push({id: i, play: play,});
+    }
+    
+    const sortedGenres = [...genreTotal.entries()].sort((a, b) => b[1] - a[1]).map(([genre]) => genre);
+    
+    const answer = [];
+    
+    for (const genre of sortedGenres) {
+        const sortedSongs = songsByGenre.get(genre).sort((a, b) => {
+            if (b.play !== a.play) return b.play - a.play; 
+            return a.id - b.id;});
+        
+        answer.push(...sortedSongs.slice(0, 2).map(song => song.id));
+    }
+    
+    return answer;
+}
